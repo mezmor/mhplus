4dcd192cad03b63338e596dd75d3708eb4623a6a
Radmire <ryancadmire@gmail.com>
2019-11-14 15:33:23 -0600

    Large Backend Commit
    First big commit from me is quite large.
    
    -Added some backend type stuff like server.js which will use express and node to route around our backend.
    
    -the main react app sits in src still and is untouched.
    
    -added connection information for both the mongo db and riot api.
    
    -added assets that we will need to refer to when rendering the cards in our replays.
    
    -used npm to add mongoose(easy db connecting and data retrieval) and nodemon(reloads app whenever changes are saved.) You can see these reflected in package.json
    
    -added some placeholder code for GETs and PUTs from our mongoDB. in the routes folder.
    
    -added a data model placeholder that will become objects we extract from the db like Card or Champion or Spell. Sky is the limit here as long as it makes sense.

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+//export our mongo Atlas key so we can connect to the DB
+//DB connection string gets passed to Mongoose to connect to our Mongo DB
+//API key can be appended to LoR REST calls as a query param ie: ?api_key = {key} OR
+//as a header ie: "X-Riot-Token": {key}
+module.exports = {
+    mongoURI: 'mongodb+srv://radmire:bSR06CiGspuyE2F1@lor-mhp-5riqw.azure.mongodb.net/test?retryWrites=true&w=majority',
+    riotAPI: 'RGAPI-93e0d111-6c27-4b19-9fe7-33007720d1a6'
+}
--- server.js
+++ server.js
@@ -1,13 +1,38 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const path = require('path');
+
+const data = require('./routes/api/data');
 const app = express();
-const port = 9001;
+const port = 9001;    //port the node server will be running on
 
-app.use(express.static(path.join(__dirname, 'build')));
+//BodyParser Middleware
+app.use(bodyParser.json());
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+//DB Config
+const db = require('./config/keys').mongoURI;
+
+//Connect to Mongo
+mongoose
+    .connect(db)
+    .then(() => console.log('Connected Successfully to db.'))
+    .catch(err => console.log(err));
+
+    
+//Set up Routes
+app.use('/api/data', data);
+
+//when we are done we will need to host this somewhere real
+//this block of code will make it so we don't have to change 
+//our code when we do an 'npm run build' instead of an 'npm start'
+if (process.env.NODE_ENV === 'production'){
+  app.use(express.static('src/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'src', 'build', 'index.html'));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server listening at ${port}`);
