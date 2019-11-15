f7a14e4abd43889ec0c058207b721620ccb96339
Lucas Pfister <lepswiss@gmail.com>
2019-11-15 12:31:42 -0800

    Little clean-up.

--- routes/api/data.js
+++ routes/api/data.js
@@ -1,12 +1,12 @@
 //This will be the way we interface with our DB.
 //Essentially a little api for requesting info
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 //Data Model
 //this is temporary and will need to be replaced once we know
 //what data we want our object responses from the DB to return
-const Data = require('../../models/dataModel');
+const Data = require("../../models/dataModel");
 
 ////////////////////// Database API //////////////////////////////////////////
 //UPDATE AS NEEDED
@@ -14,17 +14,17 @@ const Data = require('../../models/dataModel');
 //GET api/data
 //Generic get with no filter
 //Public
-router.get('/', (req, res) => {
-    Data.find()  //returns a promise
-        .then(data => res.json(data)) //returns the requested data from Mongoose as a JSON
+router.get("/", (req, res) => {
+  Data.find() //returns a promise
+    .then(data => res.json(data)); //returns the requested data from Mongoose as a JSON
 });
 
 //POST api/data
 //Generic post with no specific data to write
 //Public (if we get authenication we will need to authenicate)
-router.post('/', (req, res) => {
-    const newItem = new Data({});
-    newItem.save().then(item => res.json(item));
+router.post("/", (req, res) => {
+  const newItem = new Data({});
+  newItem.save().then(item => res.json(item));
 });
 
 //Can add new various endpoints(beyond simply /) and request type as necessary(PUT, PACTH, DELETE) etc.
