db06180aee88bbf987cbc51b2b14117cfed5470b
Lucas Pfister <lepswiss@gmail.com>
2019-11-15 12:31:58 -0800

    Fix server spinup. Lil code cleanup.

--- server.js
+++ server.js
@@ -7,37 +7,23 @@ const data = require('./routes/api/data');
 const app = express();
 const port = 9001;    //port the node server will be running on
 
-//BodyParser Middleware
-app.use(bodyParser.json());
-
-//DB Config
+//DB Setup
 const db = require('./config/keys').mongoURI;
-
-//Connect to Mongo
 mongoose
-    .connect(db)
-    .then(() => console.log('Connected Successfully to db.'))
-    .catch(err => console.log(err));
+  .connect(db)
+  .then(() => console.log('Connected Successfully to db.'))
+  .catch(err => console.log(err));
 
-    
-//Set up Routes
+//App Setup
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+});
 app.use('/api/data', data);
-
-//when we are done we will need to host this somewhere real
-//this block of code will make it so we don't have to change 
-//our code when we do an 'npm run build' instead of an 'npm start'
-if (process.env.NODE_ENV === 'production'){
-  app.use(express.static('src/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'src', 'build', 'index.html'));
-  });
-}
 
 app.listen(port, () => {
   console.log(`Server listening at ${port}`);
 });
-
-console.log("Nonblocking");
 
 /* Spawn datasource listeners */
