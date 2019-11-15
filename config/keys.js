a9b800039e5bc9bd10db77c1712ef0d04b39fb45
Lucas Pfister <lepswiss@gmail.com>
2019-11-15 10:47:04 -0800

    jslint formatting

--- config/keys.js
+++ config/keys.js
@@ -3,6 +3,7 @@
 //API key can be appended to LoR REST calls as a query param ie: ?api_key = {key} OR
 //as a header ie: "X-Riot-Token": {key}
 module.exports = {
-    mongoURI: 'mongodb+srv://radmire:bSR06CiGspuyE2F1@lor-mhp-5riqw.azure.mongodb.net/test?retryWrites=true&w=majority',
-    riotAPI: 'RGAPI-93e0d111-6c27-4b19-9fe7-33007720d1a6'
-}
+  mongoURI:
+    "mongodb+srv://radmire:bSR06CiGspuyE2F1@lor-mhp-5riqw.azure.mongodb.net/test?retryWrites=true&w=majority",
+  riotAPI: "RGAPI-93e0d111-6c27-4b19-9fe7-33007720d1a6"
+};
--- server.js
+++ server.js
@@ -1,43 +1,39 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const path = require('path');
+import express, { static } from "express";
+import { connect } from "mongoose";
+import { json } from "body-parser";
+import { resolve } from "path";
 
-const data = require('./routes/api/data');
+const data = require("./routes/api/data").default.default;
 const app = express();
-const port = 9001;    //port the node server will be running on
+const port = 9001; //port the node server will be running on
 
 //BodyParser Middleware
-app.use(bodyParser.json());
+app.use(json());
 
 //DB Config
-const db = require('./config/keys').mongoURI;
+import { mongoURI as db } from "./config/keys";
 
 //Connect to Mongo
-mongoose
-    .connect(db)
-    .then(() => console.log('Connected Successfully to db.'))
-    .catch(err => console.log(err));
+connect(db)
+  .then(() => console.log("Connected Successfully to db."))
+  .catch(err => console.log(err));
 
-    
 //Set up Routes
-app.use('/api/data', data);
+app.use("/api/data", data);
 
 //when we are done we will need to host this somewhere real
-//this block of code will make it so we don't have to change 
+//this block of code will make it so we don't have to change
 //our code when we do an 'npm run build' instead of an 'npm start'
-if (process.env.NODE_ENV === 'production'){
-  app.use(express.static('src/build'));
+if (process.env.NODE_ENV === "production") {
+  app.use(static("src/build"));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'src', 'build', 'index.html'));
+  app.get("*", (req, res) => {
+    res.sendFile(resolve(__dirname, "src", "build", "index.html"));
   });
 }
 
 app.listen(port, () => {
   console.log(`Server listening at ${port}`);
 });
-
-console.log("Nonblocking");
 
 /* Spawn datasource listeners */
