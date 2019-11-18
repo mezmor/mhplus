2d49fa7a8c44f4d4035d9426081267818f532ee1
Radmire <ryancadmire@gmail.com>
2019-11-18 01:37:03 -0600

    Updates to React App
    -updated app.js to dynamically create a grid information. This will eventually become decks and deck stats.
    
    -Updated app.css so I can actually fucking read the text.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
 import React from 'react';
 import './App.css';
-import Accordion from 'react-bootstrap/Accordion'; //import the Accordion
-import Card from 'react-bootstrap/Card';           //import the Card
 import 'bootstrap/dist/css/bootstrap.min.css';     //import the bootstrap stylings
+import Container from 'react-bootstrap/Container'; //import the bootstrap Container. to hold our data
+import Row from 'react-bootstrap/Row';             //import the bootstrap Row; one for each deck played
+import Col from 'react-bootstrap/Col';             //import the bootstrap Container; this will hold the deck info
+
 
 
 //the main location where all of the components of our application will come together.
 //since we are using react-bootstrap and should be doing very little in the way of
 //custom components most of it will be directly here and not in other files.
 function App() {
+  
+  let deckInfo = [{"name": "TeemoDeck", "data": "2-0"}, {"name": "EzrealDeck", "data": "0-1"}]; //create the array that will hold our information on the decks used
+
   return (
     <div className="App">
       <header className="App-header">
-        <Accordion>
-          <Card>
-            <Accordion.Toggle as={Card.Header} eventKey="0">
-              Deck Identifier Here
-            </Accordion.Toggle>
-            <Accordion.Collapse eventKey="0">
-              <Card.Body>Deck info here</Card.Body>
-            </Accordion.Collapse>
-          </Card>
-        </Accordion>
+        <Container>
+          <Row>
+            <Col>Deck</Col>
+            <Col>Deck Information</Col>
+          </Row>
+          {deckInfo.map(renderDeckInfo)}
+        </Container>
       </header>
     </div>
   );
-}
+};
+
+function renderDeckInfo(info, index){
+  return (
+    <Row>
+      <Col>{info.name}</Col>
+      <Col>{info.data}</Col>
+    </Row>
+  );
+};
 
 export default App;
