35b829e9246e50f0270db38cc2dbbf6fe4b07742
Lucas Pfister <lepswiss@gmail.com>
2019-11-18 01:28:40 -0800

    Fix the styling issue

--- src/App.js
+++ src/App.js
@@ -1,29 +1,61 @@
-import React from 'react';
-import './App.css';
-import Accordion from 'react-bootstrap/Accordion'; //import the Accordion
-import Card from 'react-bootstrap/Card';           //import the Card
-import 'bootstrap/dist/css/bootstrap.min.css';     //import the bootstrap stylings
-
+import React from "react";
+import "./App.css";
+import Accordion from "react-bootstrap/Accordion"; //import the Accordion
+import Card from "react-bootstrap/Card"; //import the Card
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Carousel from "react-bootstrap/Carousel";
+import "bootstrap/dist/css/bootstrap.min.css"; //import the bootstrap stylings
 
 //the main location where all of the components of our application will come together.
 //since we are using react-bootstrap and should be doing very little in the way of
 //custom components most of it will be directly here and not in other files.
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Accordion>
-          <Card>
-            <Accordion.Toggle as={Card.Header} eventKey="0">
-              Deck Identifier Here
-            </Accordion.Toggle>
-            <Accordion.Collapse eventKey="0">
-              <Card.Body>Deck info here</Card.Body>
-            </Accordion.Collapse>
-          </Card>
-        </Accordion>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Carousel>
+          <Carousel.Item>
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </Row>
+      <Row>
+        <Col xs={3}>
+          
+        </Col>
+        <Col>
+          <Accordion>
+            <Card>
+              <Accordion.Toggle as={Card.Header} eventKey="0">
+                Deck Identifier Here
+              </Accordion.Toggle>
+              <Accordion.Collapse eventKey="0">
+                <Card.Body>Deck info here</Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          </Accordion>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
