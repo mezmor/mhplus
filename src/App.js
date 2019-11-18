d9d1c3e38e7365be6824122f9fb5d19b81540fc4
Radmire <ryancadmire@gmail.com>
2019-11-17 23:19:23 -0600

    Small front end push
    Dipping my toes into react.
    
    The changes I made should run and compile with npm start.
    
    -modified index.js to remove the sample app and render the real app
    
    -modified app.js to actually do something. in this case render the bootstrap elements.
    
    -added bootstrap and react-bootstrap to package.json

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import './App.css';
+import Accordion from 'react-bootstrap/Accordion'; //import the Accordion
+import Card from 'react-bootstrap/Card';           //import the Card
+import 'bootstrap/dist/css/bootstrap.min.css';     //import the bootstrap stylings
 
+
+//the main location where all of the components of our application will come together.
+//since we are using react-bootstrap and should be doing very little in the way of
+//custom components most of it will be directly here and not in other files.
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Accordion>
+          <Card>
+            <Accordion.Toggle as={Card.Header} eventKey="0">
+              Deck Identifier Here
+            </Accordion.Toggle>
+            <Accordion.Collapse eventKey="0">
+              <Card.Body>Deck info here</Card.Body>
+            </Accordion.Collapse>
+          </Card>
+        </Accordion>
       </header>
     </div>
   );
